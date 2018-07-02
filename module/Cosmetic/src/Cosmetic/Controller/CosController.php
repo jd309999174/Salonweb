58e9c404aa8f028fe8cb8207b75ac7f9e07d8314
jiangdong <309999174@qq.com>
2018-07-02 21:59:53 +0800

    1.android去除salnumber的apk更改，2.layout加上禁止ios缩放的meta,3.美容师对顾客ios远程通知id改为固定123，4.后台通知增加ios远程通知

--- module/Cosmetic/src/Cosmetic/Controller/CosController.php
+++ module/Cosmetic/src/Cosmetic/Controller/CosController.php
@@ -2669,8 +2669,46 @@ class CosController extends AbstractActionController
             $form->setData($request->getPost());
             if ($form->isValid()) {
                 $task->setSalnumber($id);
-                $this->getNotificationinfoMapper()->saveTask($task);
     
+                //增加ios通知 1.取出全部deviceToken，循环发送
+                $customers=$this->getCustomerMapper()->getCustomer($id);
+                foreach ($customers as $customer){
+                    $deviceToken=$customer->getCustoken();
+                    $passphrase='j12321456';
+                    $message=$task->getNotitle().":".$task->getNocontent();
+                    $ctx = stream_context_create();
+                    stream_context_set_option($ctx, 'ssl', 'allow_self_signed', true);
+                    stream_context_set_option($ctx, 'ssl', 'verify_peer', false);
+                    stream_context_set_option($ctx, 'ssl', 'local_cert', 'public/pushcertificate/123/customerios.pem');
+                    stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+                    
+                    // Open a connection to the APNS server
+                    //这个为正是的发布地址
+                    $fp = stream_socket_client('ssl://gateway.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT, $ctx);
+                    //这个是沙盒测试地址，发布到appstore后记得修改哦
+                    //$fp = stream_socket_client('ssl://gateway.sandbox.push.apple.com:2195', $err,$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+                    
+                    
+                    // Create the payload body
+                    $body['aps'] = array(
+                        'alert' => $message,
+                        'sound' => 'default',
+                        "badge" => 1
+                    );
+                    
+                    // Encode the payload as JSON
+                    $payload = json_encode($body);
+                    
+                    // Build the binary notification
+                    $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+                    
+                    // Send it to the server
+                    $result = fwrite($fp, $msg, strlen($msg));
+                    
+                    fclose($fp);
+                }
+                
+                $this->getNotificationinfoMapper()->saveTask($task);
                 // Redirect to list of tasks
                 return $this->redirect()->toRoute('cosmetic', array(
                     'action' => 'Notificationinfo'
