8b5b29ae170cf149640636aeb513e1d7ba011722
jiangdong <309999174@qq.com>
2018-07-22 03:00:26 +0800

    修复产品编辑无法提交的问题（submit的id和name不能为submit）

--- module/Cosmetic/src/Cosmetic/Product/ProductForm.php
+++ module/Cosmetic/src/Cosmetic/Product/ProductForm.php
@@ -297,7 +297,7 @@ class ProductForm extends Form
         
         
         $this->add(array(
-            'name' => 'submit',
+            'name' => 'submitname',
             'attributes' => array(
                 'type' => 'submit',
                 'value' => '提交',
